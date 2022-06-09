8f976e905dd3f2987e99328006c3036db509b90e
Crowdbotics <team@crowdbotics.com>
2022-06-09 12:11:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "death_racers_35650",
+  url: "https://death_racers_35650.botics.co",
+  api: "https://death_racers_35650.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
